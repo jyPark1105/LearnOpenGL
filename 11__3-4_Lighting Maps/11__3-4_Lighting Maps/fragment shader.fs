1073ece944c8c1bef179dcad10f5b207aecd57ce
Jeyoung Park <128760195+jyPark1105@users.noreply.github.com>
2024-12-20 19:27:02 +0900

    Add files via upload

---
+++ 11__3-4_Lighting Maps/11__3-4_Lighting Maps/fragment shader.fs
@@ -0,0 +1,45 @@
+#version 330 core
+out vec4 FragColor;
+
+struct Material {           // 참고로 diffuse랑 ambient랑 값이 거의 동일해서 걍 생략함
+    sampler2D diffuse;      // 유니폼 샘플러2D
+    sampler2D specular;    
+    float shininess;
+}; 
+
+struct Light {
+    vec3 position;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+in vec3 FragPos;  
+in vec3 Normal;  
+in vec2 TexCoords;
+  
+uniform vec3 viewPos;
+uniform Material material;
+uniform Light light;
+
+void main()
+{
+    // ambient, 그냥 오브젝트에 대한 diffuse값을 ambient값으로 사용함
+    vec3 ambient = light.ambient * texture(material.diffuse, TexCoords).rgb;
+  	
+    // diffuse 
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(light.position - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexCoords).rgb;  
+    
+    // specular
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);  
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec3 specular = light.specular * spec * texture(material.specular, TexCoords).rgb;  
+        
+    vec3 result = ambient + diffuse + specular;
+    FragColor = vec4(result, 1.0);
+} 
